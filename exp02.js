76ae113f1dd4054f9552e4f96b93d7ec87553819
Harshita Tripathi <148742116+HarshitaTripathii@users.noreply.github.com>
2025-08-16 15:18:00 +0000

    ejs: html +superPower

--- exp02.js
+++ exp02.js
@@ -1,6 +1,17 @@
 const express= require("express");
 const app=express();
 
+app.set("view engine", "ejs");
+
+app.use((req, res, next)=>{
+    console.log("heya , i am a middleware : app.use()")
+    next();
+
+});
+
+app.get("/krishna", (req,res)=>{
+    res.render("index1")
+})
 app.get("/", (req, res)=>{
     res.send("yes, this is the / page");
 })
@@ -9,11 +20,7 @@ app.get("/engineer/profile/:username", (req,res)=>{
     res.send(` hello ${req.params.username} `);
 })
 
-app.use((req, res, next)=>{
-    console.log("heya , i am a middleware : app.use()")
-    next();
 
-});
 
 
 app.listen(3000);
